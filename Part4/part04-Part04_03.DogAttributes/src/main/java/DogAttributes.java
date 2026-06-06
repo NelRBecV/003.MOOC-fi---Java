8302d8f956b4315976c1630d5ade0b2dc86c1ab8
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-06 12:47:52 -0300

    Add files via upload

---
+++ Part4/part04-Part04_03.DogAttributes/src/main/java/DogAttributes.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class DogAttributes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Follow the instructions carefully
+        // and run the tests.
+        /*System.out.println("Dog's Name; ");
+        String dogName = scanner.nextLine();
+        System.out.println("Dog's Breed; ");
+        String dogBreed = scanner.nextLine();
+        System.out.println("Dog's Age; ");
+        int dogAge = Integer.valueOf(scanner.nextLine());*/
+        
+        
+    }
+}
