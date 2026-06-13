c22c4fd775160caf3dad62a8120ed67f87720538
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:40:24 -0300

    Update CountingToHundred.java
    Adding spaces between lines

--- Part2/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ Part2/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class CountingToHundred {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);        
+        Scanner scanner = new Scanner(System.in);
+        
         for(int number = Integer.valueOf(scanner.nextLine());number <= 100; number++){
             System.out.println(number);
         }
