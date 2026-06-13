b90cd9abb7c6df6b4e814b78e4e64b6d65096fa5
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:43:31 -0300

    Update Factorial.java
    Adding spaces between lines

--- Part2/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ Part2/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,12 +5,15 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Give a number:");
-        int num = Integer.valueOf(scanner.nextLine());
+        int num = Integer.valueOf(scanner.nextLine());        
         int fact = 1;
+        
         for (int n = 1; n <= num; n++){
             fact *= n;
         }
+        
         System.out.println("Factorial: " + fact);
     }   
 }
