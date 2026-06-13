9e849a63ef00be5b75ad66614d50b8b361ded0a4
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:59:53 -0300

    Update RememberTheseNumbers.java
    Adding spaces between lines and moving ArrayList declaration up

--- Part3/part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
+++ Part3/part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
@@ -6,8 +6,8 @@ public class RememberTheseNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Integer> numbers = new ArrayList<>();
+        
         while (true) {
             int luku = Integer.valueOf(scanner.nextLine());
             if (luku == -1) {
