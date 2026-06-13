091ba9e6f168de2c48b6d96453273c7ac5b53ee7
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 20:13:12 -0300

    Update Program.java
    Adding comments to main

--- Part4/part04-Part04_23.CreatingANewFile/src/main/java/Program.java
+++ Part4/part04-Part04_23.CreatingANewFile/src/main/java/Program.java
@@ -2,5 +2,7 @@ public class Program {
 
     public static void main(String[] args) {
         // you don't need to do anything here...
+
+        //this excercise was aimed to familiarize ourselves with java file manager
     }
 }
