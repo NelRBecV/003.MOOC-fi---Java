51293f6e4436c23e580cc8c8abf6185c0e899ee5
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 20:09:50 -0300

    Update NumberOfStrings.java
    Adding spaces between lines

--- Part4/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ Part4/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -6,6 +6,7 @@ public class NumberOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
+        
         while(true){
             String input = scanner.nextLine();
             if(input.equals("end")){
