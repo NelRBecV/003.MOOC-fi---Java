4f689e3c75365f04eae745d633e495baae5d8587
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:32:42 -0300

    Update SquareRootOfSum.java
    Adding comments and spaces between lines

--- Part2/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ Part2/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class SquareRootOfSum {
 
     public static void main(String[] args) {
+        //1. type in two numbers
         Scanner scanner = new Scanner(System.in);
         int first = Integer.valueOf(scanner.nextLine());
         int second = Integer.valueOf(scanner.nextLine());
-        int sum = first + second;
-        /*double squareRoot =  Math.sqrt(sum);
         
-        System.out.println(Math.round(squareRoot));*/
+        int sum = first + second;
+        
         System.out.println(Math.sqrt(first + second));
     }
 }
