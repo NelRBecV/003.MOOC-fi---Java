b5e8cc2074fd8c15d3b5a03161946f3a3023cec3
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 19:14:12 -0300

    Update Login.java
    Adding spaces between lines

--- Part3/part03-Part03_25.Login/src/main/java/Login.java
+++ Part3/part03-Part03_25.Login/src/main/java/Login.java
@@ -6,11 +6,14 @@ public class Login {
     public static void main(String[] args) {
         String[] users = {"alex","emma"};
         String[] pswrd = {"sunshine","haskell"};
+        
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter username: ");
+        System.out.println("Enter username: ");        
         String user = scanner.nextLine();
-        System.out.println("Enter password: ");
+        
+        System.out.println("Enter password: ");        
         String pass = scanner.nextLine();
+        
         for (int i = 0; i < 2; i++){
             if (user.equals(users[i]) && pass.equals(pswrd[i])){
                 System.out.println("You have successfully logged in!");
