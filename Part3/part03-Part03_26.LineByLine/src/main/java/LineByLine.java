f50aa18327a1a6120cec6a36b39b71517108228b
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 19:14:55 -0300

    Update LineByLine.java
    Adding spaces between lines

--- Part3/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ Part3/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,11 +5,13 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         while (true){
             String text = scanner.nextLine();
             if (text.isEmpty()){
                 break;
             }
+        
             String[] words = text.split(" ");
             for (String word : words){
                 System.out.println(word);
