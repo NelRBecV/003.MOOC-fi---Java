6d84d35ed3f44ef69ae3895427397d4eebdb6b6e
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:46:31 -0300

    Update Reprint.java
    Adding spaces between lines

--- Part2/part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ Part2/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -10,6 +10,7 @@ public class Reprint {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many times?");
         int times = Integer.valueOf(scanner.nextLine());
+        
         for (int i=0; i < times; i++){
             printText();
         }
