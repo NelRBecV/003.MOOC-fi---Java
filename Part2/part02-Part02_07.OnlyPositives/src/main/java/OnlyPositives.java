0cfcc8b8a07a7d8d20463a8bc960251f90825e7d
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:35:29 -0300

    Update OnlyPositives.java
    Adding spaces between lines

--- Part2/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ Part2/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,9 +5,11 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         while (true){
             System.out.println("Give a number:");
             int number = Integer.valueOf(scanner.nextLine());
+            
             if (number > 0){
                 System.out.println(number * number);
             } else if (number == 0) {
