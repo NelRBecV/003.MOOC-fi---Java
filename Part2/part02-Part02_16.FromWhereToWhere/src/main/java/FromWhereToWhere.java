8f4734847abd47c8f994497eb9be15e893a8e400
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:41:40 -0300

    Update FromWhereToWhere.java
    Adding spaces between lines

--- Part2/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ Part2/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -9,8 +9,10 @@ public class FromWhereToWhere {
         // Write your program here
         System.out.println("Where to?");
         int whereTo = Integer.valueOf(scanner.nextLine());
+        
         System.out.println("Where from?");
         int whereFrom = Integer.valueOf(scanner.nextLine());
+        
         for (int i = whereFrom; i <= whereTo; i++){
             System.out.println(i);
         }
