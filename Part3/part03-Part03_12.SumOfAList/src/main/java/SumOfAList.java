58ded92f57eed9a397a20acdd4c90ff89ba11dfa
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 19:07:29 -0300

    Update SumOfAList.java
    moving ArrayList declaration up

--- Part3/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
+++ Part3/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -6,8 +6,8 @@ public class SumOfAList {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Integer> list = new ArrayList<>();
+        
         while (true) {
             int input = Integer.valueOf(scanner.nextLine());
             if (input == -1) {
