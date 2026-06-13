32d46696631523da518c4ac0838f4fa67e406d48
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:36:23 -0300

    Update NumberOfNegativeNumbers.java
    Adding spaces between lines

--- Part2/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ Part2/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,9 +6,11 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
+        
         while (true) {
-            System.out.println("Give a number: ");
+            System.out.println("Give a number: ");            
             int negative = Integer.valueOf(scanner.nextLine());
+            
             if (negative > 0) {
                 continue;
             } else if (negative == 0){
