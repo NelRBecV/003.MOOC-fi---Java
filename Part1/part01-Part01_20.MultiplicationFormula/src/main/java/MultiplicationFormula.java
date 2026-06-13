e8d268cf09dbe98ddb8158750fac64bccf1bdba1
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 09:35:28 -0300

    Update MultiplicationFormula.java
    Adding blank lines among print-input pairs

--- Part1/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ Part1/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -9,9 +9,12 @@ public class MultiplicationFormula {
         // Write your program here
         System.out.println("Give the first number: ");
         int first = Integer.valueOf(scanner.nextLine());
+        
         System.out.println("Give the second number: ");
         int second = Integer.valueOf(scanner.nextLine());
+        
         int result = first * second;
+        
         System.out.println(first + " * " + second + " = " + result);
     }
 }
