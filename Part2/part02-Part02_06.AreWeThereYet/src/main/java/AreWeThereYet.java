9adfe310554a25a5747590b628973707a31643b2
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:34:59 -0300

    Update AreWeThereYet.java
    Adding spaces between lines

--- Part2/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ Part2/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -7,6 +7,7 @@ public class AreWeThereYet {
         Scanner scanner = new Scanner(System.in);
         int goal = 4;
         int userNumber = 0; 
+        
         while (userNumber != goal){
             System.out.println("Give a number:");
             userNumber = Integer.valueOf(scanner.nextLine());            
