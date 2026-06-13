ba0c62fc2253133a30b90c32e49ec64d1028cf24
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:38:50 -0300

    Update AverageOfNumbers.java
    Adding spaces between lines

--- Part2/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ Part2/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -7,16 +7,21 @@ public class AverageOfNumbers {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
+        
         while (true) {
             System.out.println("Give a number: ");
             int number = Integer.valueOf(scanner.nextLine());
+            
             if (number == 0) {
                 break;                        
             }
+            
             sum += number;
             count += 1;
         }
+        
         double average = sum / (double) count;
+        
         System.out.println("Average of the numbers: "+ average);
     }
 }
