61eb92efee6d04dd28d30e1dd333db0517b30fd5
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:53:32 -0300

    Update ThirdElement.java
    Adding spaces between lines and moving ArrayList declaration to up

--- Part3/part03-Part03_01.ThirdElement/src/main/java/ThirdElement.java
+++ Part3/part03-Part03_01.ThirdElement/src/main/java/ThirdElement.java
@@ -6,8 +6,8 @@ public class ThirdElement {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<String> list = new ArrayList<>();
+        
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("")) {
