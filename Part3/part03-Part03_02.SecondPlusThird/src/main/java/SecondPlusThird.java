5ba5bc6f1f3e906d8812b24ee3cce57fdb91d1b7
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 18:54:35 -0300

    Update SecondPlusThird.java
    Adding spaces between lines and moving ArrayList declaration up

--- Part3/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
+++ Part3/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -6,8 +6,8 @@ public class SecondPlusThird {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Integer> numbers = new ArrayList<>();
+        
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
             if (number == 0) {
