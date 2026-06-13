f2fae7798eec11c1d7e59bd41cc7dfbc1b87c614
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 09:36:00 -0300

    Update AverageOfTwoNumbers.java
    Adding blank lines among print-input pairs

--- Part1/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ Part1/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -9,9 +9,12 @@ public class AverageOfTwoNumbers {
         // Write your program here
         System.out.println("Give the first number: ");
         int first = Integer.valueOf(scanner.nextLine());
+        
         System.out.println("Give the second number: ");
         int second = Integer.valueOf(scanner.nextLine());
+        
         double average = (first + (double) second) / 2;
+        
         System.out.println("The average is " + average);
     }
 }
