8a3c7f42ada20f2eb95243dc10fc15063e31723c
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 19:00:52 -0300

    Update OnlyTheseNumbers.java
    Adding spaces between lines and moving ArrayList declaration up

--- Part3/part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ Part3/part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -6,8 +6,8 @@ public class OnlyTheseNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Integer> numbers = new ArrayList<>();
+        
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
             if (number == -1) {
@@ -18,8 +18,10 @@ public class OnlyTheseNumbers {
         }
         System.out.println("From where? ");
         int start = Integer.valueOf(scanner.nextLine());
+        
         System.out.println("To where? ");
         int end = Integer.valueOf(scanner.nextLine());
+        
         for (int num = start; num < end + 1; num++){
             System.out.println(numbers.get(num));
         }
