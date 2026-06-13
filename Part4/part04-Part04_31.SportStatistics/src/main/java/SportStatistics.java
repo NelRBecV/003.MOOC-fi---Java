590f2949333f08cb10b94e32f0e9e1c61b0128ba
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 20:19:34 -0300

    Update SportStatistics.java
    Adding spaces between lines

--- Part4/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ Part4/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -8,21 +8,27 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Team> teams = new ArrayList<>();        
+        ArrayList<Team> teams = new ArrayList<>(); 
+       
         System.out.println("File: ");
         String file = scan.nextLine();
+       
         System.out.println("Team: ");
         String team = scan.nextLine().strip();
+       
         try {
             Scanner data = new Scanner(Paths.get(file));
             Team teamSearch = new Team(team);
+           
             while(data.hasNextLine()){
                 //System.out.println(data.nextLine());
                 String currentData = data.nextLine();
+               
                 if (currentData.contains(team)){                    
                     String[] teamInfo = currentData.split(",");
                     int score1 = Integer.valueOf(teamInfo[2]);
                     int score2 = Integer.valueOf(teamInfo[3]);
+                   
                     if (team.equals(teamInfo[0])){                        
                         teamSearch.addMatch(score1,score2);                        
                     } else if (team.equals(teamInfo[1])){                        
