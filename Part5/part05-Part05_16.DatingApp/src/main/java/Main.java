090cca5424261b86081a35aab541f6b5fd1d8a6a
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 21:48:31 -0300

    Update Main.java
    Deleting some blank spaces

--- Part5/part05-Part05_16.DatingApp/src/main/java/Main.java
+++ Part5/part05-Part05_16.DatingApp/src/main/java/Main.java
@@ -4,22 +4,16 @@ public class Main {
     public static void main(String[] args) {
         // test your program here!
         
-        SimpleDate now = new SimpleDate(4,1,2026);
-        
+        SimpleDate now = new SimpleDate(4,1,2026);        
         System.out.println("Current Date: " + now);
         
-        now.advance();
-               
+        now.advance();               
         System.out.println("Date after passing 5 days: " + now);
         
-        now.advance(10);
-        
+        now.advance(10);        
         System.out.println("Date after passing 10 more days: " + now);
         
-        SimpleDate advance = now.afterNumberOfDays(50);
-        
+        SimpleDate advance = now.afterNumberOfDays(50);        
         System.out.println("Date in 50 days in future: " + advance);
-        
-        
     }
 }
