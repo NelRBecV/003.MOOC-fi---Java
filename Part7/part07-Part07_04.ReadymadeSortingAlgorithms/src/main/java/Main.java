7bd66f48b2226980893cdbe3df7b5fd1fd04abfd
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-14 11:04:15 -0300

    Update Main.java
    Add some blank spaces

--- Part7/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ Part7/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -7,11 +7,14 @@ public class Main {
         // insert test code here
         int[] array1 = {3, 1, 5, 99, 3, 12};
         ArrayList<Integer> array2 = new ArrayList<>();
+        
         for (int i : array1){
             array2.add(i);
         }
+        
         String[] array3 = {"sometimes","I","think","about","killing","myself"};
         ArrayList<String> array4 = new ArrayList<>();
+        
         for (String i: array3){
             array4.add(i);
         
