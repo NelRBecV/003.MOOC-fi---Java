2f0e42b5d6ff93009ec5905f382adcaf5a2f8017
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-14 10:50:36 -0300

    Update MainProgram.java
    Write clarification comments in main. Add a blank line between comment lines an return keyword

--- Part6/part06-Part06_13.Exercises/src/main/java/MainProgram.java
+++ Part6/part06-Part06_13.Exercises/src/main/java/MainProgram.java
@@ -3,6 +3,9 @@ public class MainProgram {
 
     // update here your exercise progress
     public static int partsCompleted() {
+        // This main method is intended to show how many part of the excercises you did only.
+        // Either Excercise.java or ExcerciseManagement.java calling were not allowed by JUnit test
+        
         return 2;
     }
 }
