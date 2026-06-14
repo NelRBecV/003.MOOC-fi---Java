20d12203e7e1faddb38aa8ab7f2b9118a54f6c04
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-13 21:47:20 -0300

    Update Main.java

--- Part5/part05-Part05_15.Archive/src/main/java/Main.java
+++ Part5/part05-Part05_15.Archive/src/main/java/Main.java
@@ -7,18 +7,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<Item>archive = new ArrayList<>();
+        
         while(true){
             System.out.println("Idetifier? (empty will stop)");
             String code = scan.nextLine();
+        
             if(code.isBlank()){
                 break;
             }
+            
             System.out.println("Name? (empty will stop)");
             String desc = scan.nextLine();
+            
             if (desc.isBlank()){
                 break;
             }
-            Item item = new Item(code,desc);
+            
+            Item item = new Item(code,desc);            
             if (!(archive.contains(item))){
                 archive.add(item);
             }
