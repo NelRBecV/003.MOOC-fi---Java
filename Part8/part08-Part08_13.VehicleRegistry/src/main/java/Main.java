50e8c23f099d19b5178e75439d21e4009486820e
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-14 22:05:33 -0300

    Update Main.java
    Add some blank spaces. Rearrange comment lines and rease extra space in code lines

--- Part8/part08-Part08_13.VehicleRegistry/src/main/java/Main.java
+++ Part8/part08-Part08_13.VehicleRegistry/src/main/java/Main.java
@@ -19,11 +19,12 @@ public class Main {
         registration.add(li2, "Rudolf");
         registration.add(li3, "Jurgen");
         registration.add(li4, "Marcel");
-        // if the equals-method hasn't been overwritten, the same license number will be added to the list againg       
         
+        // if the equals-method hasn't been overwritten, the same license number will be added to the list againg       
         registration.add(new LicensePlate("FI", "ABC-123"), "Benjen");
         System.out.println("owners:");
         registration.printOwners();
+        
         // if the hasCode-method hasn't been overwritten, the owners won't be found
         System.out.println("");
         System.out.println("License Plates:");
