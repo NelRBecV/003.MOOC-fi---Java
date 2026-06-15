2b13ae0ecafa7bad4ab4be3cfc4f125f1fc9278e
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-14 21:19:18 -0300

    Update AverageOfPositiveNumbers.java
    Add some blank spaces

--- Part8/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Part8/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -7,20 +7,25 @@ public class AverageOfPositiveNumbers {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
+        
         while(true){            
             int number = Integer.valueOf(scanner.nextLine()); 
+            
             if(number == 0){
                 break;
             }
+            
             if (number > 0){
                 sum += number;
                 count++;
             }
         }
+        
         if(count == 0){
             System.out.println("Cannot calculate the average");
             return;
         }
+        
         System.out.println((double)sum/count);
     }
 }
