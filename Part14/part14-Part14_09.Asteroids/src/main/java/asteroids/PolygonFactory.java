184f618ac82e942ac95a586df708a52ff4f9cf69
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 20:12:19 -0300

    Update PolygonFactory.java
    Add some comments to the code

--- Part14/part14-Part14_09.Asteroids/src/main/java/asteroids/PolygonFactory.java
+++ Part14/part14-Part14_09.Asteroids/src/main/java/asteroids/PolygonFactory.java
@@ -17,13 +17,15 @@ public class PolygonFactory {
         Random rnd = new Random();
         
         double size = 10 + rnd.nextInt(10);
-        
+
+        //Create a new Polygon and set pentagon angles
         Polygon polygon = new Polygon();
         double c1 = Math.cos(Math.PI * 2 / 5);
         double c2 = Math.cos(Math.PI / 5);
         double s1 = Math.sin(Math.PI * 2 / 5);
         double s2 = Math.sin(Math.PI * 4 / 5);
-        
+
+        //Building an irregular pentagon (asteroid) by using mathematic formula to draw pentagons
         polygon.getPoints().addAll(
                 size, 0.0,
                 size * c1, -1 * size * s1,
@@ -31,7 +33,8 @@ public class PolygonFactory {
                 -1 * size * c2, size * s2,
                 size * c1, size * s1
         );
-        
+
+        //Setting asteroid random position in the matrix
         for(int i = 0; i < polygon.getPoints().size(); i++){
             int change = rnd.nextInt(5) - 2;
             polygon.getPoints().set(i, polygon.getPoints().get(i) + change);
