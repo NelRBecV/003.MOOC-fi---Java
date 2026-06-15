9f932eb59a90163554b4a42a9663c5b1b2417372
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 20:53:03 -0300

    Update Character.java
    Add comments to the class Character methods

--- Part14/part14-Part14_09.Asteroids/src/main/java/asteroids/Character.java
+++ Part14/part14-Part14_09.Asteroids/src/main/java/asteroids/Character.java
@@ -28,23 +28,28 @@ public class Character {
         
         this.alive = true;
     }
-    
+
+    //Return a custom Polygon object
     public Polygon getCharacter(){        
         return this.character;
     }
-    
+
+    //Turns the Character to the left
     public void turnLeft(){
         this.character.setRotate(this.character.getRotate() - 5);
     }
-    
+
+    //Turns the character to the right
     public void turnRight(){
         this.character.setRotate(this.character.getRotate() + 5);
     }
-    
+
+    //moves the character forward
     public void move(){
         this.character.setTranslateX(this.character.getTranslateX() + movement.getX());
         this.character.setTranslateY(this.character.getTranslateY() + movement.getY());
-        
+
+        //Checks if the chatacter go beyond boundaires and reset its position inside window measurement
         if (this.character.getTranslateX() > AsteroidsApplication.WIDTH){
             this.character.setTranslateX(this.character.getTranslateX() % AsteroidsApplication.WIDTH);            
         } else if (this.character.getTranslateX() < -5){        
@@ -57,7 +62,8 @@ public class Character {
             this.character.setTranslateY(-5);
         }
     }
-    
+
+    //Set randomly movement of the object in the space (window)
     public void accelerate(){
         double changeX = Math.cos(Math.toRadians(this.character.getRotate()));
         double changeY = Math.sin(Math.toRadians(this.character.getRotate()));
@@ -67,24 +73,29 @@ public class Character {
         
         this.movement = this.movement.add(changeX, changeY);
     }
-    
+
+    //Checks if this object impacts with other
     public boolean collide(Character other){
         Shape collisionArea = Shape.intersect(this.character, other.getCharacter());
         return collisionArea.getBoundsInLocal().getWidth() != -1;
     }
-    
+
+    //Adjust object position by adding new (X,Y) position to the object
     public void setMovement(Point2D newMovement){
         this.movement = newMovement;
     }
-    
+
+    //Returns the exact point position in the 2D graphic
     public Point2D getMoement(){
         return this.movement;
     }
-    
+
+    //Returns if the objects is still active in the game
     public boolean isAlive(){
         return this.alive;
     }
     
+    //Sets if the object is active or destroyed
     public void setAlive(boolean alive){
         this.alive = alive;
     }
