1713d7ecf190a2deb90d5e82eec66325b23f77b7
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 12:15:22 -0300

    Update Program.java
    Export customized packages and declare variable objects from them

--- Part11/part11-Part11_09.TheThreePackages/src/main/java/Program.java
+++ Part11/part11-Part11_09.TheThreePackages/src/main/java/Program.java
@@ -1,11 +1,17 @@
 
 import java.util.Scanner;
+import a.A
+import b.B
+import c.C
 
 public class Program {
 
+    //Creating my own packages
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your classes here
-
+        A newA = new A();
+        B newB = new B();
+        C newC = new C();
     }
 }
