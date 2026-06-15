3522bbfb6ff7da1220b3b2584b2954c36b675f62
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 13:27:29 -0300

    Update JokeApplication.java
    Rearrange code in overriden start, createBorderFrame and main

--- Part13/part13-Part13_11.Joke/src/main/java/application/JokeApplication.java
+++ Part13/part13-Part13_11.Joke/src/main/java/application/JokeApplication.java
@@ -17,21 +17,17 @@ public class JokeApplication extends Application{
     @Override
     public void start(Stage win) throws Exception {
         BorderPane mainWindow = new BorderPane();                
-        HBox menuButtons = new HBox();
-        
+        HBox menuButtons = new HBox();        
         Button joke = new Button("Joke");
         Button answer = new Button("Answer");
         Button explain = new Button("Explanation");
-       
-        
+               
         menuButtons.getChildren().addAll(joke, answer,explain);                
-        menuButtons.setSpacing(20);
-        
+        menuButtons.setSpacing(20);        
         
         mainWindow.setPadding(new Insets(20,20,20,20));        
         mainWindow.setTop(menuButtons);
-        
-        
+                
         StackPane randomJoke = createBorderFrame("What do you call a bear with no teeth?");
         StackPane jokeAnswer = createBorderFrame("A gummy bear.");
         StackPane jokeExplain = createBorderFrame("Because gummy bears have no teeth");
@@ -39,8 +35,7 @@ public class JokeApplication extends Application{
         joke.setOnAction((event) -> mainWindow.setCenter(randomJoke));
         answer.setOnAction((event) -> mainWindow.setCenter(jokeAnswer));
         explain.setOnAction((event) -> mainWindow.setCenter(jokeExplain));
-        
-        
+                
         mainWindow.setCenter(randomJoke);
                 
         Scene view = new Scene(mainWindow);
@@ -52,18 +47,17 @@ public class JokeApplication extends Application{
     public StackPane createBorderFrame(String text){
         Label labelText = new Label(text);
         StackPane labelFrame = new StackPane();
+        
         labelFrame.setPrefSize(300,180);
         labelFrame.getChildren().add(labelText);
         labelFrame.setPadding(new Insets(10,10,10,10));
-        
-                
+                        
         return labelFrame;
     }
 
 
     public static void main(String[] args) {
-        launch(JokeApplication.class);
-        
+        launch(JokeApplication.class);        
         System.out.println("Hello world!");
     }
 }
