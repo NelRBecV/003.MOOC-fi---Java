c62b9d2a70399815149c075dfeadfad4c3433cb1
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 11:37:20 -0300

    Update Program.java
    Add comment into main method

--- Part11/part11-Part11_01.Customer/src/main/java/Program.java
+++ Part11/part11-Part11_01.Customer/src/main/java/Program.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Program {   
-   
+   //Class diagrams
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your class here
