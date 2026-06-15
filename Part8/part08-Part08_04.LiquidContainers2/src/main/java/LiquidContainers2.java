96cee011ff24a2ab3ace37bb60f007875712c3f0
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-14 21:24:19 -0300

    Update LiquidContainers2.java
    Add some blank spaces

--- Part8/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ Part8/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -14,21 +14,27 @@ public class LiquidContainers2 {
             System.out.println("Second: " + second);            
             
             String input = scan.nextLine();
+            
             if (input.equals("quit")) {
                 break;
-            }            
+            }
+            
             String[] command = input.split(" ");            
             int amount = Integer.valueOf(command[1]);
+            
             if (command[0].equals("add")){
                 first.add(amount);
             }
+            
             if (command[0].equals("move")){
                 if(first.contains() < amount){
                     amount = first.contains();
                 }
+                
                 first.remove(amount);
                 second.add(amount);
             }
+            
             if (command[0].equals("remove")){
                 second.remove(amount);
             }
