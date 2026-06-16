1d1d85dbcf1ecbc964e809fe0e263b4070097550
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-15 21:28:44 -0300

    Update Todo.java
    Add some comments to the code

--- Part14/part14-Part14_10.Database/src/main/java/application/Todo.java
+++ Part14/part14-Part14_10.Database/src/main/java/application/Todo.java
@@ -1,7 +1,8 @@
 package application;
 
 public class Todo {
-
+    //Keeps track of the chores from household
+    
     private int id;
     private String name;
     private String description;
@@ -18,34 +19,42 @@ public class Todo {
         this(-1, name, description, done);
     }
 
+    //Returns an integer that indicates the number of the task
     public int getId() {
         return id;
     }
 
+    //Assign an integer that represents the number of the task
     public void setId(int id) {
         this.id = id;
     }
 
+    //Get the name of the task
     public String getName() {
         return name;
     }
 
+    //Change the current name with a new one
     public void setName(String name) {
         this.name = name;
     }
 
+    //Returns a string that describes the current taks
     public String getDescription() {
         return description;
     }
 
+    //Inserts a new description or change the current with a new value
     public void setDescription(String description) {
         this.description = description;
     }
 
+    //Returns a boolean which indicates if the task was done or not
     public Boolean getDone() {
         return done;
     }
 
+    //Set the status of a task to 'true' if the task is completed, otherwise sets as 'false'
     public void setDone(Boolean done) {
         this.done = done;
     }
