e1b692e72c5ac31f5f08534e1f2c78c72c64495c
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-07-29 10:47:24 -0300

    Add files via upload

---
+++ Part04/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account my_account = new Account("Todd",100.0);
+        
+        my_account.deposit(20);
+        
+        System.out.println(my_account);
+    }
+}
