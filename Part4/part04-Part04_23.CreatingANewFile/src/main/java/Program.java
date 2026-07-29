2993c69e4cc55b58be39dade77bbf4565ba4adb4
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-07-29 09:57:46 -0300

    Delete Part4 directory

--- Part4/part04-Part04_23.CreatingANewFile/src/main/java/Program.java
+++
@@ -1,8 +0,0 @@
-public class Program {
-
-    public static void main(String[] args) {
-        // you don't need to do anything here...
-
-        //this excercise was aimed to familiarize ourselves with java file manager
-    }
-}
